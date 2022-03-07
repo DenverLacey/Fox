f0a647d1614067cad42dabaa0c9acc810fc7b771
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2022-03-07 14:51:14 +1100

    Add build process separate to Xcode.

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+workspace "Fox"
+	configurations { "Debug", "Release" }
+
+project "Fox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	files { 
+		"*.h", 
+		"*.cpp",
+		"utfcpp/**.h"
+	}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
